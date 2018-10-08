61995d2940a927d404b087ee0b0b3fab6a1434ea
becky4141 <43335290+becky4141@users.noreply.github.com>
2018-10-08 17:29:42 +0800

    Add files via upload

---
+++ w05/menu 3.0.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int isLeap(int year){
+    if((year%4==0 && year%100!=0) || year%400==0)
+        return 1;
+	else
+		return 0;
+}
+
+int timeconvert(int hh,int mm,int ss){
+    return hh*3600+mm*60+ss;
+}
+
+int main(){
+
+    int choice,year,seconds;
+    int hh,mm,ss;
+
+    while(1){
+        printf("Main Menu\n");
+        printf("1. Leap Year\n");
+        printf("2. Time convert\n");
+        printf("3. Exit\n");
+        printf("=> ");
+        scanf("%d",&choice);
+        if(choice==3)break;
+        switch(choice){
+            case 1:
+                printf("Enter year: ");
+                scanf("%d",&year);
+                if(isLeap(year)){
+                    printf("Year %d is a leap year.\n\n",year);
+                }else{
+                    printf("Year %d is not a leap year.\n\n",year);
+                }
+
+                break;
+
+            case 2:
+                printf("Enter hh:mm:ss : ");
+                scanf("%d:%d:%d",&hh,&mm,&ss);
+                seconds=timeconvert(hh,mm,ss);
+                printf("%02d : %02d : %02d = %02d seconds\n\n",hh,mm,ss,seconds);
+                break;
+        }
+        continue;
+    }
+}
